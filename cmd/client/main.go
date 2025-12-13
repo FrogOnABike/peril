622dd42ae3379ae680622f23569a82eef4b93394
FrogOnABike <mark.skrzypczyk.com>
2025-12-13 21:32:31 +0000

    start of Ch3L5

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connectString := "amqp://guest:guest@localhost:5672/"
+	fmt.Println("Starting Peril client...")
+	conn, err := amqp.Dial(connectString)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connection successful")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down Peril client...")
 }
