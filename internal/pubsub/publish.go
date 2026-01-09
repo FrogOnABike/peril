bb4f1849dc0d0cd9f78f8d034d427932d29435ba
FrogOnABike <mark.skrzypczyk.com>
2026-01-09 21:51:42 +0000

    Start of Ch6L2

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -1,7 +1,9 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 
 	"github.com/frogonabike/peril/internal/routing"
@@ -28,6 +30,27 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        valBytes,
+		},
+	)
+	return nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var valBytes bytes.Buffer
+	enc := gob.NewEncoder(&valBytes)
+	err := enc.Encode(val)
+	if err != nil {
+		return err
+	}
+
+	ch.PublishWithContext(context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/gob",
+			Body:        valBytes.Bytes(),
 		},
 	)
 	return nil
