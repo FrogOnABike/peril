1a4bbb82f5304ea8840bacbdf751ec0358b2f409
FrogOnABike <mark.skrzypczyk.com>
2026-01-13 21:41:43 +0000

    Start of Ch6L3

--- internal/pubsub/subscribe.go
+++ internal/pubsub/subscribe.go
@@ -76,3 +76,67 @@ func SubscribeJSON[T any](
 	}()
 	return nil
 }
+
+func subscribe[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+	handler func(T) AckType,
+	unmarshaller func([]byte) (T, error),
+) error {
+
+	// Confirm the channel and queue exist
+	ch, qu, err := DeclareAndBind(conn, exchange, queueName, key, simpleQueueType)
+	if err != nil {
+		return err
+	}
+	// 	Consume messages
+	deliveries, err := ch.Consume(
+		qu.Name,
+		"",
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return err
+	}
+	// Start a goroutine to handle messages
+	go func() {
+		for d := range deliveries {
+			var val T
+			err := unmarshaller(d.Body, &val)
+			if err != nil {
+				continue
+			}
+			at := handler(val)
+			switch at {
+			case Ack:
+				if err = d.Ack(false); err != nil {
+					fmt.Printf("could not ack message: %v\n", err)
+					continue
+				}
+				fmt.Printf("acked message\n")
+			case NackRequeue:
+				if err = d.Nack(false, true); err != nil {
+					fmt.Printf("could not nackrequeue message: %v\n", err)
+					continue
+				}
+				fmt.Printf("nackrequeued message\n")
+			case NackDiscard:
+				if err = d.Nack(false, false); err != nil {
+					fmt.Printf("could not nackdiscard message: %v\n", err)
+					continue
+				}
+				fmt.Printf("nackdiscarded message\n")
+			default:
+				fmt.Printf("unknown AckType: %v\n", at)
+			}
+		}
+	}()
+	return nil
+}
